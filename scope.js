e939485be18619b578d0d1015c014e81b980f182
fahim-mota007 <hossainalamin003@gmail.com>
2021-02-02 00:37:41 +0600

    closer

---
+++ scope.js
@@ -0,0 +1,21 @@
+let bonus = 20
+
+function sum(first, second){
+        let result = first + second + bonus;
+        // console.log(bonus);
+        if(result >9){
+            var mood = "happy"
+            mood = "bad"
+            mood = "chill"
+            mood = "funky"
+            // console.log(mood);
+        }
+        var day = "friday"
+        console.log(day);
+        return result;
+}
+
+const output = sum (3,7);
+
+// console.log(output);
+// console.log(bonus);
