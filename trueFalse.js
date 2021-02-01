6bfb9999484cbf2aa08b3837314f0a7a07dc7a40
fahim-mota007 <hossainalamin003@gmail.com>
2021-02-01 23:12:34 +0600

    true vs false

---
+++ trueFalse.js
@@ -0,0 +1,17 @@
+// falsy;
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truthy
+// '0',''[]
+let name = 'false';
+if(name || name ==){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
