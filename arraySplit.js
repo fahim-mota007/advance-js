bf074068d5c39a66d7cc9994498a0d327bfc8595
fahim-mota007 <hossainalamin003@gmail.com>
2021-02-02 00:52:22 +0600

    arraySplit

---
+++ arraySplit.js
@@ -0,0 +1,10 @@
+const num = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19];
+// const part = num.slice(2 ,5);
+
+
+const removed = num.splice(2,7);
+// console.log(removed);
+// console.log(num);
+
+const together = num.join("ami")
+console.log(together);
