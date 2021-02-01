fb951d84d15f56d1b1032ee4b92435a52d2da33d
fahim-mota007 <hossainalamin003@gmail.com>
2021-02-02 00:13:08 +0600

    lurned a lot

--- objectProperty.js
+++ objectProperty.js
@@ -9,4 +9,6 @@ const students = [
 
 const names = students.map (s => s.name);
 const ids = students.map(s => s.id);
-console.log(ids);
+const bigger = students.filter( s => s.id>40);
+const biggerOne = students.find( s => s.id>40)
+console.log(biggerOne);
